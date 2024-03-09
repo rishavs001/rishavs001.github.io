537536132e1ca4b3cafdbf04865f6979f5273187
rishavs001 <risu55.2014@gmail.com>
2024-03-09 11:59:00 +0530

    updated form

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
-//emailJS
+// //emailJS
+// function SendMail(e) {
+//     e.preventDefault();
+//     // alert('Sending your message');
+   
+//     let params = {
+//       from_name : document.getElementById("fullName").value,
+//       email_id : document.getElementById("emailId").value,
+//       mobile_number : document.getElementById("mobileNumber").value,
+//       subject : document.getElementById("emailSubject").value,
+//       message : document.getElementById("message").value
+//     }
+//     emailjs.send("service_9cclw2w","template_l0ueb2k",params)
+//     .then((res) => {
+//       // console.log('Email sent successfully:', response);
+//       alert('Email sent successfully!');
+//     //   Clear the form after successful submission
+//       e.target.reset();
+//     }, (error) => {
+//       // console.error('Error sending email:', error);
+//       alert('An error occurred while sending the email.'+ error);
+//     });
+//   }
 function SendMail(e) {
-    e.preventDefault();
-    // alert('Sending your message');
-    let params = {
-      from_name : document.getElementById("fullName").value,
-      email_id : document.getElementById("emailId").value,
-      mobile_number : document.getElementById("mobileNumber").value,
-      subject : document.getElementById("emailSubject").value,
-      message : document.getElementById("message").value
-    }
-    emailjs.send("service_9cclw2w","template_l0ueb2k",params)
-    .then((res) => {
-      // console.log('Email sent successfully:', response);
-      alert('Email sent successfully!');
-    //   Clear the form after successful submission
-      e.target.reset();
-    }, (error) => {
-      // console.error('Error sending email:', error);
-      alert('An error occurred while sending the email.'+ error);
-    });
-  }
+  e.preventDefault();
+  let params = {
+      from_name: document.getElementById("fullName").value,
+      email_id: document.getElementById("emailId").value,
+      mobile_number: document.getElementById("mobileNumber").value,
+      subject: document.getElementById("emailSubject").value,
+      message: document.getElementById("message").value
+  };
+
+  emailjs.send("service_9cclw2w", "template_l0ueb2k", params)
+      .then((res) => {
+          alert('Email sent successfully!');
+          // Clear the form after successful submission
+          document.getElementById("myForm").reset();
+      })
+      .catch((error) => {
+          alert('An error occurred while sending the email: ' + error);
+      });
+}
